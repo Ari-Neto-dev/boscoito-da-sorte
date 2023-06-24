e884d825bcf8daa2703610c1f877ad619852baed
Ari Azevedo <azevedo.consultor@hotmail.com>
2023-06-24 17:26:09 -0300

    crira função de abrir biscoito

--- src/App.js
+++ src/App.js
@@ -1,39 +1,52 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import "./style.css";
 
-class App extends Component{
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
-    this.state={};
+    this.state = {
+      textoFrase: "",
+    };
 
-    this.frases=[ 
-    'A vida trará coisas boas se tiver paciência.',
-    'Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.',
-    'Não compense na ira o que lhe falta na razão.',
-    'Defeitos e virtudes são apenas dois lados da mesma moeda.',
-    'Não há que ser forte. Há que ser flexível.',
-    'Todos os dias organiza os seus cabelos, por que não faz o mesmo com o coração?',
-    'Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.',
-    'A juventude não é uma época da vida, é um estado de espírito.',
-    'Podemos escolher o que semear, mas somos obrigados a colher o que plantamos.'];
+    this.quebraBiscoito = this.quebraBiscoito.bind(this);
+
+    this.frases = [
+      "A vida trará coisas boas se tiver paciência.",
+      "Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.",
+      "Não compense na ira o que lhe falta na razão.",
+      "Defeitos e virtudes são apenas dois lados da mesma moeda.",
+      "Não há que ser forte. Há que ser flexível.",
+      "Todos os dias organiza os seus cabelos, por que não faz o mesmo com o coração?",
+      "Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.",
+      "A juventude não é uma época da vida, é um estado de espírito.",
+      "Podemos escolher o que semear, mas somos obrigados a colher o que plantamos.",
+    ];
   }
 
+  quebraBiscoito() {
+    let state = this.state;
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+    state.textoFrase = '"' + this.frases[numeroAleatorio] + '  "';
+    this.setState(state);
+  }
 
-  render(){
-    return(
-      <div>
-     <img src={require('./assets/sorte.png') }alt=""/>
-     <Botao/>
-     <h3>Frase numero 1 aleatoria...</h3>
+  render() {
+    return (
+      <div className="container">
+        <img src={require("./assets/sorte.png")} alt="" className="img" />
+        <Botao nome="Abrir biscoito" acaoBtn={this.quebraBiscoito} />
+        <h3 className="textoFrase">{this.state.textoFrase}</h3>
       </div>
     );
   }
 }
 
-class Botao extends Component{
-  render(){
-    return(
-      <button>Abrir Biscoito</button>
-
+class Botao extends Component {
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.acaoBtn}>{this.props.nome}</button>
+      </div>
     );
   }
 }
